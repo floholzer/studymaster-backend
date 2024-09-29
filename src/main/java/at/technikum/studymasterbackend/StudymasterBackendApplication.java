38d7b9c3d510dcd767e20bc3e7cba61b4f68a871
Florentin Holzer <wi22b090@fh-technikum.at>
2024-09-29 17:00:29 +0200

    init commit

---
+++ src/main/java/at/technikum/studymasterbackend/StudymasterBackendApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.studymasterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudymasterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudymasterBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/studymasterbackend/StudymasterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.studymasterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudymasterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
